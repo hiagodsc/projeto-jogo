e15f8a96308437f0f8d56dfb0cbb503f79a9198c
Marley Paranhos <mpsvideos.marley@gmail.com>
2024-01-20 20:19:37 -0300

    Atualização do projeto-jogo.py

--- projeto-jogo.py
+++ projeto-jogo.py
@@ -1,7 +1,12 @@
-def jogo_adivinhacao():
-  numero_secreto = random.randint(1, 100)
-  tentativas = 0
-  
+"""
+Trabalho em grupo
+Turma: 20
+Desafio: Jogo de Adivinha
+"""
+
 print('Jogo da Advinhação')
 
-
+def jogo_adivinhacao():
+  numero_secreto = random.randint(1, 101)
+  tentativas = 0
+  palpite = int(input('Digite seu palpite de 1 até 100: '))

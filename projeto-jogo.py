ca4e1753e15fe98e8631659f9d7d7a7ca9188e2e
f1rst <dossantos.hiago10@gmail.com>
2024-01-21 11:53:33 -0300

    atualização - numero acima ou abaixo

--- projeto-jogo.py
+++ projeto-jogo.py
@@ -1,33 +1,47 @@
 import random
 from time import sleep
 def jogo_adivinhacao():
-    while True:
-        jogador = int(input('||  Digite um número de 1 a 10: '))
-        computador = random.randint(0, 10)
+    try:
+
+        jogador = int(input('||  Digite um número de 1 a 100: '))
+        computador = random.randint(0, 100)
         
-        try: 
+        while True:
             
-            
-            #else:
-                #print(f'INCORRETO! Tente novamente.')
             if jogador < computador:
                 print(f'INCORRETO! Tente novamente.')
                 jogador = int(input('||  Digite um número MAIOR: '))
             elif jogador > computador:
                 print(f'INCORRETO! Tente novamente.')
                 jogador = int(input('||  Digite um número MENOR: '))
-                #continue
+                continue
             elif jogador == computador:
                 print(f'VOCÊ ACERTOU!')
                 break
-        except:
-            print('Digite um valor válido')
+
+    except:
+        print('||{: ^36}||'.format('VALOR INVÁLIDO!'))
+
+        print('=' * 40)
+
 
 print('=' * 40)
-print('||         JOGO DA ADIVINHAÇÃO        ||')
+print('||{: ^36}||'.format('JOGO DA ADVINHAÇÃO'))
 print('=' * 40)
+sleep(1)
 
 print('''||  Vamos jogar? Se  você  adivinhar  ||
 ||  o número que pensei, você ganha.  ||''')
 print('=' * 40)
+
+print('||{: ^36}||'.format('3...'))
+sleep(0.5)
+print('||{: ^36}||'.format('2...'))
+sleep(0.5)
+print('||{: ^36}||'.format('1...'))
+sleep(0.5)
+print('||{: ^36}||'.format('Pensei'))
+sleep(0.5)
+print('=' * 40)
+
 jogo_adivinhacao()

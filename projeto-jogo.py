c2e2bd7ea9a5147fd986a26c58232f4c4800c993
Marley Paranhos <mpsvideos.marley@gmail.com>
2024-01-20 21:05:52 -0300

    Atualização do projeto-jogo.py
    Importado a biblioteca randint.
    Inserido a variável palpite
    Corrigido o parâmetro b do randint(a=1, b=100) para b=101, tendo em vista que no parâmetaro b o número é exclusive, e estamos sorteando de 1 até 100.

--- projeto-jogo.py
+++ projeto-jogo.py
@@ -3,10 +3,15 @@ Trabalho em grupo
 Turma: 20
 Desafio: Jogo de Adivinha
 """
+from random import randint
 
 print('Jogo da Advinhação')
 
+
 def jogo_adivinhacao():
-  numero_secreto = random.randint(1, 101)
+  numero_secreto = randint(1, 101)
   tentativas = 0
   palpite = int(input('Digite seu palpite de 1 até 100: '))
+
+
+jogo_adivinhacao()

e99d2d8f5b7ad2fcf72a48e91711fce48cd67e8a
“Agda <agdacavalcanti09@gmail.com>
2024-01-23 16:02:27 -0300

    Código adicionado no arquivo projeto-jogo.py

--- projeto-jogo.py
+++ projeto-jogo.py
@@ -1,17 +1,38 @@
 """
 Trabalho em grupo
 Turma: 20
-Desafio: Jogo de Adivinha
+Desafio: Jogo de Adivinhação
 """
 from random import randint
 
-print('Jogo da Advinhação')
+print('\n\nJogo da Adivinhação')
+print('Adivinhe o número secreto!')
 
 
 def jogo_adivinhacao():
+
+  a = True
   numero_secreto = randint(1, 101)
   tentativas = 0
-  palpite = int(input('Digite seu palpite de 1 até 100: '))
+
+  while a == True:  
+
+    palpite = int(input("Digite seu palpite de 1 até 100: "))
+
+    if palpite == numero_secreto:
+      print("\nParabéns")
+      print("Você acertou!")
+      print("O número secreto é: ", numero_secreto)
+      a = False
+
+    elif palpite > numero_secreto: # Orienta se o número secreto é maior ou menor que o palpite
+      print("\nVocê errou!")
+      print("Tente um número menor\n")
+
+    else:
+      print("\nVocê errou!")
+      print("Digite um número maior\n")
+  
 
 
 jogo_adivinhacao()

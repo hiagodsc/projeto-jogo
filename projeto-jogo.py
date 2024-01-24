ffa06f143285664488993e40403f486c18c47025
Marley <mpsvideos.marley@gmail.com>
2024-01-24 11:29:02 -0300

    Adicionado número de tentativas

--- projeto-jogo.py
+++ projeto-jogo.py
@@ -8,31 +8,43 @@ Desafio: Desenvolver um jogo de adivinha
 from random import randint
 from time import sleep
 
+
 # função do jogo de adivinhação de um número
 def jogo_adivinhacao():
     try:
+        # número de tentativas
+        tentativas = 1
+
         # solicita ao usuário que digite um número de 1 até 100
         jogador = int(input('||  Digite um número de 1 a 100: '))
-        computador = randint(0, 100)
-        
-        
-        while True:            
-            if jogador < computador:
+
+        computador = randint(1, 100)
+
+        while tentativas < 5:
+
+            if jogador == computador:
+                print(f'VOCÊ ACERTOU!')
+                print('FIM DE JOGO!')
+
+            elif jogador < computador:
                 print(f'INCORRETO! Tente novamente.')
                 jogador = int(input('||  Digite um número MAIOR: '))
+
             elif jogador > computador:
                 print(f'INCORRETO! Tente novamente.')
                 jogador = int(input('||  Digite um número MENOR: '))
-                continue
-            elif jogador == computador:
-                print(f'VOCÊ ACERTOU!')
+
+            tentativas += 1
+
+            if tentativas == 5:
+                print()
+                print(f'Você atingiu o número máximo de tentativas.')
                 print('FIM DE JOGO!')
-                break
 
     except:
         print('||{: ^36}||'.format('VALOR INVÁLIDO!'))
 
-        print('=' * 40)
+    print('=' * 40)
 
 
 print('=' * 40)
@@ -41,7 +53,10 @@ print('=' * 40)
 sleep(1)
 
 print('''||  Vamos jogar? Se  você  adivinhar  ||
-||  o número que pensei, você ganha.  ||''')
+||  O número que pensei, você ganha.  ||
+||  Você tem cinco tentativas.        ||''')
+
+print('|| ')
 print('=' * 40)
 
 print('||{: ^36}||'.format('3...'))

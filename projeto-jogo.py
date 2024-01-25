995d2ec655d0bd5f490a52630ba6b203df773fce
f1rst <dossantos.hiago10@gmail.com>
2024-01-24 23:01:48 -0300

    projeto atualizado

--- projeto-jogo.py
+++ projeto-jogo.py
@@ -7,49 +7,80 @@ Desafio: Desenvolver um jogo de adivinha
 
 import random
 from time import sleep
+
+# função do jogo de adivinhação de um número entre 1 e 100
 def jogo_adivinhacao():
-    try:
+    while True:
+        try:
+            # Número de tentativas
+            tentativas = 1
 
-        jogador = int(input('||  Digite um número de 1 a 100: '))
-        computador = random.randint(0, 100)
+            # Solicitação ao usuário de um número de 1 até 100
+            jogador = int(input('||          Digite um número de 1 a 100: '))
+            
+            computador = random.randint(1, 100)
 
-        while True:
+            while tentativas < 15:
+                if jogador == computador:
+                    # Definição dos níveis das tentativas
+                    if tentativas == 1:
+                        print(f'||    Você acertou em {tentativas} tentativa! VOCÊ É SÊNIOR!   ||')
+                    elif tentativas <= 5: 
+                        print(f'||   Você acertou em {tentativas} tentativas! VOCÊ É SÊNIOR!   ||')
+                    elif tentativas <= 10:
+                        print(f'||    Você acertou em {tentativas} tentativas! VOCÊ É PLENO!   ||')
+                    else:
+                        print(f'||   Você acertou em {tentativas} tentativas! VOCÊ É JÚNIOR!   ||')
 
-            if jogador < computador:
-                print('||{: ^36}||'.format('INCORRETO! Tente Novamente.'))
-                jogador = int(input('||    Digite um número MAIOR: '))
-            elif jogador > computador:
-                print('||{: ^36}||'.format('INCORRETO! Tente Novamente.'))
-                jogador = int(input('||    Digite um número MENOR: '))
-                continue
-            elif jogador == computador:
-                print('||{: ^36}||'.format('VOCÊ ACERTOU!'))
-                print('=' * 40)
+                    print('||{: ^50}||'.format('FIM DE JOGO!'))
+                    print('=' * 54)
+                    break
+
+                elif jogador < computador:
+                    print('||{: ^50}||'.format('INCORRETO! Tente Novamente.'))
+                    jogador = int(input('||          Digite um número MAIOR: '))
+                elif jogador > computador:
+                    print('||{: ^50}||'.format('INCORRETO! Tente Novamente.'))
+                    jogador = int(input('||          Digite um número MENOR: '))
+                    
+                
+                tentativas += 1
+
+                if tentativas == 15:
+                    print('||{: ^50}||'.format('Você atingiu o número máximo de tentativas.'))
+                    print('||{: ^50}||'.format('O computador é o vencedor!'))
+                    print('||{: ^50}||'.format('FIM DO JOGO!'))
+                    print('=' * 54)
+            
+            jogar_novamente = input('||          Deseja jogar novamente? (S/N): ').upper()
+            if jogar_novamente != 'S':
+                print('||{: ^50}||'.format('FIM DO JOGO!'))
+                print('=' * 54)
                 break
 
-    except:
-        print('||{: ^36}||'.format('VALOR INVÁLIDO!'))
+        except:
+            print('||{: ^50}||'.format('VALOR INVÁLIDO!'))
+            print('=' * 54)
 
-        print('=' * 40)
 
-
-print('=' * 40)
-print('||{: ^36}||'.format('JOGO DA ADVINHAÇÃO'))
-print('=' * 40)
+print('=' * 54)
+print('||{: ^50}||'.format('JOGO DA ADVINHAÇÃO'))
+print('=' * 54)
 sleep(1)
 
-print('''||  Vamos jogar? Se  você  adivinhar  ||
-||  o número que pensei, você ganha.  ||''')
-print('=' * 40)
+print('||{: ^50}||'.format('Vamos jogar? Se  você  adivinhar'))
+print('||{: ^50}||'.format('o número que pensei, você ganha.'))
+print('||{: ^50}||'.format('Você tem 15 tentativas!'))
+print('=' * 54)
 
-print('||{: ^36}||'.format('3...'))
+print('||{: ^50}||'.format('3...'))
 sleep(0.5)
-print('||{: ^36}||'.format('2...'))
+print('||{: ^50}||'.format('2...'))
 sleep(0.5)
-print('||{: ^36}||'.format('1...'))
+print('||{: ^50}||'.format('1...'))
 sleep(0.5)
-print('||{: ^36}||'.format('Pensei'))
+print('||{: ^50}||'.format('Pensei'))
 sleep(0.5)
-print('=' * 40)
+print('=' * 54)
 
 jogo_adivinhacao()
